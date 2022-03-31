cf2f269aad92ff5660de183d60a2c890be9026be
FARIS137 <96936492+FARIS137@users.noreply.github.com>
2022-03-31 20:00:28 +0700

    Update class_tujuan.php

--- praktikum 05 php/class_dispenser/class_tujuan.php
+++ praktikum 05 php/class_dispenser/class_tujuan.php
@@ -3,24 +3,36 @@ require_once 'class_dispenser.php';
 
 class tujuan extends dispenser{
     public $customer;
+    public $sisauang;
 
-function __construct($volume,$hargasegelas,$volumegelas,$namaminuman,$customer ){
+function __construct($volume,$hargasegelas,$volumegelas,$namaminuman,$customer,$sisauang ){
     parent::__construct($volume, $hargasegelas, $volumegelas, $namaminuman);
     $this->customer = $customer;
+    $this->sisauang = $sisauang;
 }
 
+    public function sisa($uang){
+        $this->sisauang = $this->sisauang - $uang;
+    
     public function nambah($ab,$jumlah){
         $ab->isi($jumlah);
     }
     public function beli($ab,$jumlah){
         $ab->isi($jumlah);
         $this->isigelas($jumlah);
-       
+    }
+        function uang($ab,$uang){
+        $ab->sisa($uang);
+        $this->Nama($uang);
+    }
+    function bayar($ab,$uang){
+        $ab->sisa($uang);
     }
 
     public function cetak(){
         parent::cetak();
         echo '<br/>Nama : '.$this->customer;
+        echo '<br/> Sisa uang : '.$this->sisauang;
     }
 }
 
